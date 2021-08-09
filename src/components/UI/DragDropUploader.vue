2ec7c7795a77c28920973faf95816d025212ba77
Zhang Qiao <zqthedesigner@gmail.com>
2021-08-10 01:13:53 +0800

    uploader ui update

--- src/components/UI/DragDropUploader.vue
+++ src/components/UI/DragDropUploader.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="n-drag-drop-uploader">
     <q-uploader>
-      <template v-slot:header="scope">
-        <div class="row bg-white">
-          <div>
+      <template v-slot:header>
+        <div class="row bg-white justify-center">
+          <div class="full-width">
             <q-uploader-add-trigger />
             <div
-              class="items-center content-center q-pa-sm q-ma-md no-wrap"
-              :class="scope.files.length === 0 ? 'column' : 'row'"
+              class="
+                items-center
+                content-center
+                q-pa-sm q-ma-md
+                no-wrap
+                column
+                s-round-sm
+              "
               :style="`border: 2px dashed ${color}50`"
             >
               <q-icon
                 name="mdi-cloud-upload-outline"
                 :style="`opacity: 0.3; color:${color}`"
-                :size="scope.files.length === 0 ? '6rem' : '2rem'"
+                size="6rem"
               />
               <p
                 class="q-my-sm q-mx-sm"
@@ -30,19 +36,32 @@
           <div
             v-for="file in scope.files"
             :key="file.name"
-            class="row items-center justify-between full-width no-wrap"
+            class="
+              row
+              items-center
+              justify-between
+              no-wrap
+              s-border
+              q-mx-md q-mb-md q-pa-sm
+            "
           >
-            <!-- Thumbnails -->
-            <div>
+            <!-- Thumbnails and icons -->
+            <div class="col-2 column items-center">
               <img
                 v-if="file.__img"
                 :src="file.__img.src"
-                style="width: 60px"
+                style="width: 100%"
+              />
+              <q-icon
+                v-else
+                :name="getFileIconName(file)"
+                :style="`color:${color}`"
+                size="1.8rem"
               />
             </div>
 
             <!-- File name and Progress Bar -->
-            <div class="column full-width q-px-md">
+            <div class="col-8 column q-pl-sm">
               <p class="q-mb-xs q-mt-none" style="word-break: break-all">
                 {{ file.name }}
               </p>
@@ -54,11 +73,12 @@
             </div>
 
             <!-- Remove File -->
-            <div>
+            <div class="col-2 column items-center">
               <q-icon
                 name="mdi-trash-can-outline"
                 @click="scope.removeFile(file)"
                 size="sm"
+                class="n-delete-file-icon"
               />
             </div>
           </div>
@@ -68,15 +88,38 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
+
+interface fileInterface {
+  type: string
+}
 
 export default defineComponent({
   name: 'NewComponent',
 
   props: { color: { type: String, default: '#0277BD' } },
+
   setup() {
-    return {}
+    function getFileIconName(file: fileInterface) {
+      /** Returns file's icon name in MDI icon library */
+      const type = file.type.split('/')
+      console.log(type)
+      let icon = 'mdi-file-star' // Default
+
+      type[0] === 'audio'
+        ? (icon = 'mdi-file-music')
+        : type[0] === 'text'
+        ? (icon = 'mdi-file-document')
+        : type[0] === 'video'
+        ? (icon = 'mdi-file-video')
+        : type[1] === 'pdf' // check type[1] from this line onwards
+        ? (icon = 'mdi-file-pdf')
+        : null
+
+      return icon
+    }
+    return { getFileIconName }
   }
 })
 </script>
@@ -84,5 +127,20 @@ export default defineComponent({
 <style lang="scss">
 .n-drag-drop-uploader .q-uploader__dnd {
   height: 0px;
+}
+.n-drag-drop-uploader .q-uploader__list {
+  min-height: 0px;
+  padding: 0px;
+}
+.n-drag-drop-uploader .q-uploader {
+  max-height: clamp(320px, 60vh, 700px);
+  width: 100%;
+  max-width: 800px;
+}
+.n-delete-file-icon {
+  color: $grey-7;
+}
+.n-delete-file-icon:hover {
+  color: $red-4;
 }
 </style>
